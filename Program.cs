3573f6ee17e9c5dde2605401fdf83810ff44a7b0
Vishvas Vaglay <vishvas0419@gmail.com>
2026-09-14 12:11:49 +0530

    UC3: add contacts to address book list

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using AddressBookApp.Exceptions;
 using AddressBookApp.Models;
+using AddressBookApp.Services;
 using AddressBookApp.Validation;
+using System.Diagnostics.Contracts;
 using System.Text;
 
 namespace AddressBookApp
@@ -9,22 +11,29 @@ namespace AddressBookApp
     {
         static void Main(string[] args)
         {
+
+            Contact contact1 = new Contact("John", "Doe", "12 MG Road", "Pune", "Maharashtra", "411001", "9876543210", "john.doe@mail.com");
+            //Contact contact2 = new Contact("john", "Doe", "12 MG Road", "Pune", "Maharashtra", "411001", "9876543210", "john.doe@mail.com");
             try
             {
-                Contact contact1 = new Contact("John", "Doe", "12 MG Road", "Pune", "Maharashtra", "411001", "9876543210", "john.doe@mail.com");
-                Contact contact2 = new Contact("john", "Doe", "12 MG Road", "Pune", "Maharashtra", "411001", "9876543210", "john.doe@mail.com");
 
                 ContactValidator.Validate(contact1);
                 Console.WriteLine(contact1);
 
-                ContactValidator.Validate(contact2);
-                Console.WriteLine(contact2);
-
+                //ContactValidator.Validate(contact2);
+                //Console.WriteLine(contact2);
             } 
             catch(InvalidContactException ex)
             {
                 Console.WriteLine($"Error : {ex.Message}");
+                return;
             }
+
+            //UC-3 
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact(contact1);
+            //addressBook.AddContact(contact2);
+            addressBook.PrintAll();
         }
     }
 }
---
+++ Services/AddressBook.cs
@@ -0,0 +1,26 @@
+﻿using AddressBookApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookApp.Services
+{
+    internal class AddressBook
+    {
+        private List<Contact> contacts = new();
+        private Contact contact;
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+            Console.WriteLine("Contact added successfully");
+        }
+
+        public void PrintAll()
+        {
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+    }
+}
