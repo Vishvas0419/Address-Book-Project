e6faa66e9cd4e6d0ac9044bf73b5ffe81699736a
Vishvas Vaglay <vishvas0419@gmail.com>
2026-09-14 10:14:45 +0530

    UC1: create Contact class with ToString

---
+++ Models/Contact.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace AddressBookApp.Models
+{
+    internal class Contact
+    {
+        //first name, last name, address, city, state, zip, phone number, 
+        //email, and a constructor that sets all of them
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public string Address {  get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip {  get; set; }
+
+        public string PhoneNumber {  get; set; }
+
+        public string Email {  get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} | {Address}, " + $"{City}, {State} {Zip} | {PhoneNumber} | {Email}";
+        }
+
+        public Contact(string firstName, string lastName, string address, string city, string state, string zip, string phonNumber,string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City= city;
+            State = state;
+            Zip = zip;
+            PhoneNumber = phonNumber;
+            Email = email;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace AddressBookApp
+﻿using AddressBookApp.Models;
+using System.Text;
+
+namespace AddressBookApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Contact contact1 = new Contact("John", "Doe", "12 MG Road", "Pune", "Maharashtra", "411001", "9876543210", "john.doe@mail.com");
+            Console.WriteLine(contact1.ToString());
+            
         }
     }
 }
